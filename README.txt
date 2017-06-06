ce162d6e6824587e09a0d8af022562f6a7aae81e
xuwei <xuwei@xu-wei.cn>
2017-06-06 16:57:14 +0800

    20170606

--- README.txt
+++ README.txt
@@ -1,1 +1,2 @@
 《利用Python进行数据分析》(Python for Data Analysis)书中例子Python3的实现。原书例子是基于Python2.7编写的。
+原书数据来源地址：https://github.com/wesm/pydata-book 。
--- usagov.py
+++ usagov.py
@@ -1,6 +1,8 @@
 import json
 import  numpy as np
-import  pandas as np
+import  pandas as pd
+import matplotlib.pyplot as plt
+
 from  pandas import  DataFrame,Series
 
 def get_counts(sequence):
@@ -12,11 +14,51 @@ def get_counts(sequence):
             counts[x]=1
     return (counts)
 
-path = 'E:\\pydata-book-master\\ch02\\usagov_bitly_data2012-03-16-1331923249.txt'
-with open(path , 'r') as f:
-    data0=[json.loads(line) for line in f]
-time_zones = [rec['tz'] for rec in data0 if 'tz' in rec]
-counts=get_counts(time_zones)
-print(counts)
-print(len(time_zones))
+if __name__ == '__main__':
+    # 输入文件路径，注意\用\\转义
+    path = 'E:\\pydata-book-master\\ch02\\usagov_bitly_data2012-03-16-1331923249.txt'
 
+    #打开文件
+    with open(path , 'r') as f:
+       data0=[json.loads(line) for line in f]
+    time_zones = [rec['tz'] for rec in data0 if 'tz' in rec]
+    counts=get_counts(time_zones)
+    print(counts)
+    print(len(time_zones))
+
+
+    frame = DataFrame(data0)
+    print(frame['tz'].value_counts())
+    clean_tz = frame['tz'].fillna("Missing")
+    clean_tz[clean_tz == ''] =  'Unkown'
+    tz_counts=clean_tz.value_counts()
+#    print(tz_counts)
+    tz_counts[:10].plot(kind='barh',rot=0)
+    plt.show()
+
+    # dropna方法删除缺失数据,x.split()按空格切割字符串
+    results= Series([x.split()[0]  for x in frame.a.dropna()])
+#    print(results)
+    a_counts=results.value_counts()
+#    print(a_counts)
+    a_counts[:10].plot(kind='barh',rot=0)
+    plt.show()
+
+    cframe=frame[frame.a.notnull()]
+    operating_system = Series(np.where(cframe['a'].str.contains('Windows'),'Windows','Not Windows')) #Series
+#    print(operating_system[:5])
+
+    by_tz_os = cframe.groupby(['tz',operating_system])
+    agg_couts=by_tz_os.size().unstack().fillna(0)
+#    print(agg_couts[:10])
+
+    indexer = agg_couts.sum(1).argsort()
+#    print(indexer[:10])
+    counts_subset=agg_couts.take(indexer)[-10:]
+    print(counts_subset)
+    counts_subset.plot(kind='barh',stacked=True)
+
+    normed_subset=counts_subset.div(counts_subset.sum(1),axis=0)
+    normed_subset.plot(kind='barh',stacked=True)
+
+    plt.show()
